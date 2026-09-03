244bc56ec759ca22f885ae97f0f8944f1afcf549
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-03 18:02:54 -0400

    Add RHD 3DEN configuration module settings

--- addons/rhd_lifeserver/config.cpp
+++ addons/rhd_lifeserver/config.cpp
@@ -19,10 +19,7 @@ class CfgRemoteExec {
         class RHD_fnc_refineResult { allowedTargets = 1; jip = 0; };
         class RHD_fnc_setPrice { allowedTargets = 2; jip = 0; };
     };
-    class Commands {
-        mode = 0;
-        jip = 0;
-    };
+    class Commands { mode = 0; jip = 0; };
 };
 
 class CfgFunctions {
@@ -68,6 +65,33 @@ class CfgFunctions {
             class initMenus { postInit = 1; };
             class menuAction {};
             class findNearbyResource {};
+        };
+        class Eden {
+            file = "RHD_LifeServer\functions\eden";
+            class moduleInit {};
+            class applyConfig {};
+        };
+    };
+};
+
+class Cfg3DEN {
+    class Mission {
+        class Scenario {
+            class RHD_LifeCore {
+                displayName = "RHD LifeCore Configuration";
+                class Attributes {
+                    class farmingHarvestMin { displayName="Farming minimum harvest"; typeName="NUMBER"; defaultValue=2; expression="_this setVariable ['farmingHarvestMin',_value,true]"; };
+                    class farmingHarvestMax { displayName="Farming maximum harvest"; typeName="NUMBER"; defaultValue=5; expression="_this setVariable ['farmingHarvestMax',_value,true]"; };
+                    class miningHarvestMin { displayName="Mining minimum harvest"; typeName="NUMBER"; defaultValue=2; expression="_this setVariable ['miningHarvestMin',_value,true]"; };
+                    class miningHarvestMax { displayName="Mining maximum harvest"; typeName="NUMBER"; defaultValue=6; expression="_this setVariable ['miningHarvestMax',_value,true]"; };
+                    class civiliansAtOnePlayer { displayName="Civilians at one player"; typeName="NUMBER"; defaultValue=115; expression="_this setVariable ['civiliansAtOnePlayer',_value,true]"; };
+                    class minimumCivilians { displayName="Minimum civilian population"; typeName="NUMBER"; defaultValue=60; expression="_this setVariable ['minimumCivilians',_value,true]"; };
+                    class maximumCivilians { displayName="Maximum civilian population"; typeName="NUMBER"; defaultValue=115; expression="_this setVariable ['maximumCivilians',_value,true]"; };
+                    class populationScaleWithPlayers { displayName="Scale civilians with players"; typeName="BOOL"; defaultValue=1; expression="_this setVariable ['populationScaleWithPlayers',_value,true]"; };
+                    class harvestCooldown { displayName="Harvest cooldown (seconds)"; typeName="NUMBER"; defaultValue=2; expression="_this setVariable ['harvestCooldown',_value,true]"; };
+                    class dynamicPricing { displayName="Enable dynamic pricing"; typeName="BOOL"; defaultValue=1; expression="_this setVariable ['dynamicPricing',_value,true]"; };
+                };
+            };
         };
     };
 };
