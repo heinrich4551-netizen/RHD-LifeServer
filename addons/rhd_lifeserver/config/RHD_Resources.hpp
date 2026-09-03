85dd7e144d27101c7c2ccfdf454d667b87565199
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-03 18:24:52 -0400

    Align RHD resources with requested economy items

--- addons/rhd_lifeserver/config/RHD_Resources.hpp
+++ addons/rhd_lifeserver/config/RHD_Resources.hpp
@@ -2,14 +2,16 @@ class RHD_Resources {
     class Farming {
         class Apple { displayName="Apples"; item="apple"; min=2; max=5; illegal=0; process=""; };
         class Peach { displayName="Peaches"; item="peach"; min=2; max=5; illegal=0; process=""; };
-        class Cannabis { displayName="Cannabis"; item="cannabis"; min=1; max=3; illegal=1; process="marijuana"; };
-        class Coca { displayName="Coca Leaves"; item="cocaine_unprocessed"; min=1; max=3; illegal=1; process="cocaine"; };
+        class Grape { displayName="Grapes"; item="grape"; min=2; max=5; illegal=0; process=""; };
+        class Corn { displayName="Corn Cob"; item="corn"; min=2; max=5; illegal=0; process=""; };
+        class Cannabis { displayName="Cannabis Plant"; item="cannabis"; min=1; max=3; illegal=1; process="rhd_cannabis"; };
+        class Coca { displayName="Coca Leaf"; item="coca_leaf"; min=1; max=3; illegal=1; process="rhd_coca"; };
     };
     class Mining {
         class Iron { displayName="Iron Ore"; item="iron_unrefined"; min=2; max=6; illegal=0; process="iron"; };
         class Copper { displayName="Copper Ore"; item="copper_unrefined"; min=2; max=6; illegal=0; process="copper"; };
-        class Gold { displayName="Gold Bar"; item="goldbar"; min=1; max=2; illegal=0; process=""; };
+        class GoldOre { displayName="Gold Ore"; item="gold_ore"; min=1; max=3; illegal=0; process="rhd_gold"; };
         class Diamond { displayName="Uncut Diamond"; item="diamond_uncut"; min=1; max=2; illegal=0; process="diamond"; };
-        class Oil { displayName="Unprocessed Oil"; item="oil_unprocessed"; min=2; max=5; illegal=0; process="oil"; };
+        class OilSand { displayName="Oil Sand"; item="oil_sand"; min=2; max=5; illegal=0; process="rhd_oil"; };
     };
 };
