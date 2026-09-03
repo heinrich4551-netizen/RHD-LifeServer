d4a4233f382bce0b86f51716b5a2939b3a9c4bbc
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-03 15:13:31 -0400

    Add isolated RHD feature registry

---
+++ custom/RHD_features.hpp
@@ -0,0 +1,18 @@
+// RHD feature registry.
+// This file is intentionally independent of the upstream framework source.
+
+class RHD_Features
+{
+    farming = 1;
+    mining = 1;
+    refining = 1;
+    playerBusinesses = 1;
+    contracts = 1;
+    dynamicEconomy = 1;
+    vehicleServices = 1;
+    licenses = 1;
+    dispatch = 1;
+    events = 1;
+    civilianPopulation = 1;
+    factionProgression = 1;
+};
