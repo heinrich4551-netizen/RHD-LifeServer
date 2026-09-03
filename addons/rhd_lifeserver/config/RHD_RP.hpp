5f6a3508b169c353d1ba0c2efba02e10f9f22f02
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-03 18:30:15 -0400

    Add RHD RP configuration

---
+++ addons/rhd_lifeserver/config/RHD_RP.hpp
@@ -0,0 +1,17 @@
+class RHD_RP {
+    enabled = 1;
+    dispatch = 1;
+    evidence = 1;
+    warrants = 1;
+    impounds = 1;
+    businesses = 1;
+    licenses = 1;
+    vehicleServices = 1;
+    courts = 1;
+    government = 1;
+    taxes = 1;
+    phone = 1;
+    marketplace = 1;
+    serviceRequests = 1;
+    hospitalBilling = 1;
+};
