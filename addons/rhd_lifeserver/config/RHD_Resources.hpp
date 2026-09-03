3062af23c198adf4b2ab5403e1ee8e91239cf951
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-03 16:35:58 -0400

    Add framework-compatible RHD resource configuration

---
+++ addons/rhd_lifeserver/config/RHD_Resources.hpp
@@ -0,0 +1,15 @@
+class RHD_Resources {
+    class Farming {
+        class Apple { displayName="Apples"; item="apple"; min=2; max=5; illegal=0; process=""; };
+        class Peach { displayName="Peaches"; item="peach"; min=2; max=5; illegal=0; process=""; };
+        class Cannabis { displayName="Cannabis"; item="cannabis"; min=1; max=3; illegal=1; process="marijuana"; };
+        class Coca { displayName="Coca Leaves"; item="cocaine_unprocessed"; min=1; max=3; illegal=1; process="cocaine"; };
+    };
+    class Mining {
+        class Iron { displayName="Iron Ore"; item="iron_unrefined"; min=2; max=6; illegal=0; process="iron"; };
+        class Copper { displayName="Copper Ore"; item="copper_unrefined"; min=2; max=6; illegal=0; process="copper"; };
+        class Gold { displayName="Gold Bar"; item="goldbar"; min=1; max=2; illegal=0; process=""; };
+        class Diamond { displayName="Uncut Diamond"; item="diamond_uncut"; min=1; max=2; illegal=0; process="diamond"; };
+        class Oil { displayName="Unprocessed Oil"; item="oil_unprocessed"; min=2; max=5; illegal=0; process="oil"; };
+    };
+};
