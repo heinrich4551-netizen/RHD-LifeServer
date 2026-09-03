4a9cc9461c64c0d0e3f0fcf36be84258b4eb9698
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-03 15:13:27 -0400

    Add RHD gameplay configuration schema

---
+++ config/RHD_LifeServer.hpp
@@ -0,0 +1,57 @@
+class RHD_LifeServer
+{
+    enabled = 1;
+    serverName = "RHD LifeServer";
+    map = "Altis";
+
+    class Economy
+    {
+        enabled = 1;
+        startingCash = 2500;
+        startingBank = 5000;
+        dynamicPricing = 1;
+        priceUpdateMinutes = 15;
+        priceFloorMultiplier = 0.65;
+        priceCeilingMultiplier = 1.35;
+    };
+
+    class Population
+    {
+        enabled = 1;
+        civiliansAtOnePlayer = 115;
+        minimumCivilians = 60;
+        maximumCivilians = 115;
+        scaleWithPlayers = 1;
+        despawnDistance = 1200;
+    };
+
+    class Jobs
+    {
+        enabled = 1;
+        legalJobs = 1;
+        illegalJobs = 1;
+        jobCooldownSeconds = 30;
+    };
+
+    class Factions
+    {
+        police = 1;
+        medic = 1;
+        civilian = 1;
+        gangs = 1;
+    };
+
+    class Persistence
+    {
+        enabled = 1;
+        saveIntervalMinutes = 5;
+        backupIntervalMinutes = 30;
+    };
+
+    class Events
+    {
+        enabled = 1;
+        randomEvents = 1;
+        eventIntervalMinutes = 30;
+    };
+};
