23b71aa07a1bd81196c6719a0292f67e2fa92f77
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-04 15:01:15 -0400

    Add configurable RHD RP service fees

--- addons/rhd_lifeserver/config/RHD_RP.hpp
+++ addons/rhd_lifeserver/config/RHD_RP.hpp
@@ -14,4 +14,14 @@ class RHD_RP {
     marketplace = 1;
     serviceRequests = 1;
     hospitalBilling = 1;
+
+    class Fees {
+        treatment = 500;
+        impoundRelease = 500;
+        inspection = 100;
+        repair = 750;
+        oil = 250;
+        tires = 500;
+        full = 1500;
+    };
 };
