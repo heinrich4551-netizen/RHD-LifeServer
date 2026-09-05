dee95b5dea3117bcf037b483d98aa1e7223472ff
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-04 22:07:36 -0400

    Register scoped Antistasi compatibility function

--- addons/rhd_lifeserver/config.cpp
+++ addons/rhd_lifeserver/config.cpp
@@ -54,6 +54,7 @@ class CfgFunctions {
         class UI { file = "RHD_LifeServer\functions\ui"; class initMenus { postInit = 1; }; class menuAction {}; class findNearbyResource {}; };
         class Eden { file = "RHD_LifeServer\functions\eden"; class moduleInit {}; class applyConfig {}; };
         class Persistence { file = "RHD_LifeServer\functions\persistence"; class initPersistence {}; class loadState {}; class saveState {}; class persistenceLoop {}; };
+        class Antistasi { file = "RHD_LifeServer\functions\antistasi"; class antistasiInit { postInit = 1; }; };
         class RP {
             file = "RHD_LifeServer\functions\rp";
             class initRP {}; class dispatch {}; class dispatchAction {}; class dispatchResult {}; class createEvidence {}; class createWarrant {};
