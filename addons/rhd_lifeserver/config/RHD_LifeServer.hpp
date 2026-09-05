f2477ab06f0add8637032d49ae8daed6988ff153
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-05 07:17:01 -0400

    Fix AddonBuilder missing local LifeServer config header

---
+++ addons/rhd_lifeserver/config/RHD_LifeServer.hpp
@@ -0,0 +1,76 @@
+class RHD_LifeServer
+{
+    enabled = 1;
+    serverName = "RHD LifeServer";
+    map = "Altis";
+
+    class Economy
+    {
+        enabled = 1;
+        startingCash = 2500;
+        startingBank = 5000;
+        dynamicPricing = 1;
+        priceUpdateMinutes = 15;
+        priceFloorMultiplier = 0.65;
+        priceCeilingMultiplier = 1.35;
+        shopBuyMultiplier = 1.00;
+        shopSellMultiplier = 0.80;
+    };
+
+    class Population
+    {
+        enabled = 1;
+        civiliansAtOnePlayer = 115;
+        minimumCivilians = 60;
+        maximumCivilians = 115;
+        scaleWithPlayers = 1;
+        despawnDistance = 1200;
+    };
+
+    class HeadlessClient
+    {
+        enabled = 0;
+        useForPopulation = 0;
+        spawnBatch = 20;
+        despawnDistance = 1200;
+    };
+
+    class Jobs
+    {
+        enabled = 1;
+        legalJobs = 1;
+        illegalJobs = 1;
+        jobCooldownSeconds = 30;
+    };
+
+    class Factions
+    {
+        police = 1;
+        medic = 1;
+        civilian = 1;
+        gangs = 1;
+    };
+
+    class Persistence
+    {
+        enabled = 1;
+        saveIntervalMinutes = 5;
+        backupIntervalMinutes = 30;
+    };
+
+    class Events
+    {
+        enabled = 1;
+        randomEvents = 1;
+        eventIntervalMinutes = 30;
+        eventDurationMinutes = 10;
+    };
+
+    class Performance
+    {
+        enabled = 1;
+        logServerLoop = 0;
+        logSlowSection = 1;
+        slowSectionMilliseconds = 50;
+    };
+};
